cc2c4284853d930eb7ceb1d80ae8c4c2725ae2aa
Kassiane CG <kassianecristinagoncalves@gmail.com>
2021-11-12 02:57:22 -0300

    desafio-32 updated

--- src/dom.js
+++ src/dom.js
@@ -1,92 +1,51 @@
 ;(function (win, doc) {
 	'use strict'
 
-	const DOM = elements => {
-		if (!(this instanceof DOM)) {
-			return new DOM(elements)
-		}
+	function DOM(elements) {
+		if (!(this instanceof DOM)) return new DOM(elements)
 
-		this.element = doc.querySelectorAll(elements)
+		this.element = document.querySelectorAll(elements)
 	}
 
-	DOM.prototype.on = function on(eventType, callback) {
+	DOM.prototype.on = function on(event, callback) {
 		this.element.forEach(function (element) {
-			element.addEventListener(eventType, callback, false)
+			element.addEventListener(event, callback, false)
 		})
 	}
 
-	DOM.prototype.off = function off(eventType, callback) {
+	DOM.prototype.off = function off(event, callback) {
 		this.element.forEach(function (element) {
-			element.removeEventListener(eventType, callback)
+			element.removeEventListener(event, callback, false)
 		})
 	}
 
 	DOM.prototype.get = function get(index) {
-		if (!index) {
-			return this.element[0]
-		}
-
+		if (!index) return this.element[0]
 		return this.element[index]
 	}
 
-	DOM.prototype.forEach = function forEach() {
-		return Array.prototype.forEach.apply(this.element, arguments)
+	DOM.prototype.applyMethod = function applyMethod(method, callback) {
+		switch (method) {
+			case 'forEach':
+				return Array.prototype.forEach.call(this.element, callback)
+			case 'map':
+				return Array.prototype.map.call(this.element, callback)
+			case 'filter':
+				return Array.prototype.filter.call(this.element, callback)
+			case 'reduce':
+				return Array.prototype.reduce.call(this.element, callback)
+			case 'reduceRight':
+				return Array.prototype.reduceRight.call(this.element, callback)
+			case 'every':
+				return Array.prototype.every.call(this.element, callback)
+			case 'some':
+				return Array.prototype.some.call(this.element, callback)
+		}
 	}
 
-	DOM.prototype.map = function map() {
-		return Array.prototype.map.apply(this.element, arguments)
+	DOM.prototype.verifyType = function verifyType(param, type) {
+		return Object.prototype.toString.call(param) === '[object ' + type + ']'
 	}
 
-	DOM.prototype.filter = function filter() {
-		return Array.prototype.filter.apply(this.element, arguments)
-	}
-
-	DOM.prototype.reduce = function reduce() {
-		return Array.prototype.reduce.apply(this.element, arguments)
-	}
-
-	DOM.prototype.reduceRight = function reduceRight() {
-		return Array.prototype.reduceRight.apply(this.element, arguments)
-	}
-
-	DOM.prototype.every = function every() {
-		return Array.prototype.every.apply(this.element, arguments)
-	}
-
-	DOM.prototype.some = function some() {
-		return Array.prototype.some.apply(this.element, arguments)
-	}
-
-	DOM.isArray = function isArray(param) {
-		return Object.prototype.toString.call(param) === '[object Array]'
-	}
-
-	DOM.isObject = function isObject(param) {
-		return Object.prototype.toString.call(param) === '[object Object]'
-	}
-
-	DOM.isFunction = function isFunction(param) {
-		return Object.prototype.toString.call(param) === '[object Function]'
-	}
-
-	DOM.isNumber = function isNumber(param) {
-		return Object.prototype.toString.call(param) === '[object Number]'
-	}
-
-	DOM.isString = function isString(param) {
-		return Object.prototype.toString.call(param) === '[object String]'
-	}
-
-	DOM.isBoolean = function isBoolean(param) {
-		return Object.prototype.toString.call(param) === '[object Boolean]'
-	}
-
-	DOM.isNull = function isNull(param) {
-		return (
-			Object.prototype.toString.call(param) === '[object Null]' ||
-			Object.prototype.toString.call(param) === '[object Undefined]'
-		)
-	}
-
-	win.DOM = DOM
+	window.DOM = DOM
 })(window, document)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-;(function ($, doc) {
+;(function ($) {
 	'use strict'
 
 	/*
@@ -16,92 +16,161 @@
     - Placa
     - Cor
     - e um botão "Cadastrar"
+
   Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
   carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
   aparecer no final da tabela.
+
   Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
   empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
   Dê um nome para a empresa e um telefone fictício, preechendo essas informações
   no arquivo company.json que já está criado.
-  Essas informações devem ser adicionadas no HTML via Ajax.
+
+  Essas informações devem ser adicionadas no HTML via Ajax. - Feito
+
   Parte técnica:
   Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
+  um arquivo DOM.js. - Feito
+
   E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
+  que será nomeado de "app". - Feito
   */
 
-	var app = (() => {
+	var app = (function appController() {
 		return {
 			init: function init() {
-				console.log('app init')
 				this.companyInfo()
-				this.initEvents()
+				this.loadListCar()
+				this.saveNewCar()
 			},
 
-			initEvents() {
-				$('[data-js="form-register"]').on('submit', this.handleSubmit)
+			companyInfo: function companyInfo() {
+				var ajax = new XMLHttpRequest()
+				ajax.open('GET', 'company.json', true)
+				ajax.send()
+				ajax.onreadystatechange = this.getCompanyInfo
+			},
+
+			getElement: function getElement(element) {
+				return $(`[data-js=${element}]`).get()
+			},
+
+			getCompanyInfo: function getCompanyInfo() {
+				if (!app.requestOk.call(this)) return
+
+				var response = JSON.parse(this.responseText)
+				var $title = app.getElement('title')
+				var $tel = app.getElement('phone')
+				$title.textContent = response.name
+				$tel.textContent = response.phone
+			},
+
+			requestOk: function requestOk() {
+				return this.readyState === 4 && this.status === 200
+			},
+
+			saveNewCar: function saveNewCar() {
+				$('[data-js="formCadastro"]').on('submit', this.handleSubmit)
 			},
 
 			handleSubmit: function handleSubmit(e) {
 				e.preventDefault()
-				console.log('submit')
-				var $tableCar = $('[data-js="table-car"]').get()
-				$tableCar.appendChild(app.createNewCar())
+
+				var $url = app.getElement('urlImage').value
+				var $brand = app.getElement('brand').value
+				var $year = app.getElement('year').value
+				var $plate = app.getElement('plate').value
+				var $cor = app.getElement('color').value
+
+				var ajax = new XMLHttpRequest()
+				ajax.open('POST', 'http://localhost:3000/car')
+				ajax.setRequestHeader(
+					'Content-Type',
+					'application/x-www-form-urlencoded'
+				)
+				ajax.send(
+					`image=${$url}&brandModel=${$brand}&year=${$year}&plate=${$plate}&color=${$cor}`
+				)
+
+				ajax.onreadystatechange = function () {
+					if (ajax.readyState === 4) {
+						var $tableCar = app.getElement('table-car')
+						var count = $tableCar.children.length
+						for (let i = 0; i < count; i++) {
+							$tableCar.children[0].remove()
+						}
+						app.loadListCar()
+					}
+				}
 			},
 
-			createNewCar: function createNewCar() {
-				const $fragment = doc.createDocumentFragment()
-				var $tr = doc.createElement('tr')
-				var $tdImage = doc.createElement('td')
-				var $image = document.createElement('img')
-				var $tdBrand = doc.createElement('td')
-				var $tdYear = doc.createElement('td')
-				var $tdPlate = doc.createElement('td')
-				var $tdColor = doc.createElement('td')
+			createEl: function createEl(element) {
+				return document.createElement(element)
+			},
 
-				$image.setAttribute('src', $('[data-js="image"]').get().value)
+			removeRow: function removeRow(event) {
+				var $tr = document.getElementById(event.target.id)
+				$tr.remove()
+			},
+
+			loadListCar: function loadListCar() {
+				var ajax = new XMLHttpRequest()
+				ajax.open('GET', 'http://localhost:3000/car')
+				ajax.send()
+				ajax.onreadystatechange = this.getDataCar
+			},
+
+			getDataCar: function getDataCar() {
+				if (!app.requestOk.call(this)) return
+
+				var response = JSON.parse(this.responseText)
+				var $tableCar = app.getElement('table-car')
+				response.map(car => {
+					$tableCar.appendChild(app.createNewCar(car))
+				})
+			},
+
+			createNewCar: function createNewCar(car) {
+				var id = Math.floor(Math.random() * 1000)
+				var $fragment = document.createDocumentFragment()
+				var $tr = document.createElement('tr')
+				$tr.setAttribute('id', id)
+
+				var $tdImage = this.createEl('td')
+				var $image = this.createEl('img')
+				var $tdBrand = this.createEl('td')
+				var $tdYear = this.createEl('td')
+				var $tdPlate = this.createEl('td')
+				var $tdCor = this.createEl('td')
+				var $tdRemove = this.createEl('td')
+				var $buttonRemove = this.createEl('button')
+
+				$image.setAttribute('src', car.image)
 				$tdImage.appendChild($image)
+				$tdBrand.textContent = car.brandModel
+				$tdYear.textContent = car.year
+				$tdPlate.textContent = car.plate
+				$tdCor.textContent = car.color
 
-				$tdBrand.textContent = $('[data-js="brand-model"]').get().value
-				$tdYear.textContent = $('[data-js="year"]').get().value
-				$tdPlate.textContent = $('[data-js="plate"]').get().value
-				$tdColor.textContent = $('[data-js="color"]').get().value
+				$buttonRemove.setAttribute('type', 'button')
+				$buttonRemove.setAttribute('id', id)
+				$buttonRemove.textContent = 'remover'
+				$buttonRemove.addEventListener('click', this.removeRow)
+				$tdRemove.appendChild($buttonRemove)
 
 				$tr.appendChild($tdImage)
 				$tr.appendChild($tdBrand)
 				$tr.appendChild($tdYear)
 				$tr.appendChild($tdPlate)
-				$tr.appendChild($tdColor)
+				$tr.appendChild($tdCor)
+				$tr.appendChild($tdRemove)
 
-				return $fragment.appendChild($tr)
-			},
+				$fragment.appendChild($tr)
 
-			companyInfo: function companyInfo() {
-				console.log('company info')
-				var ajax = new XMLHttpRequest()
-				ajax.open('GET', '/company.json', true)
-				ajax.send()
-				ajax.addEventListener('readystatechange', this.getCompanyInfo, false)
-			},
-
-			getCompanyInfo: function getCompanyInfo() {
-				if (!app.isReady.call(this)) {
-					return
-				}
-
-				var data = JSON.parse(this.responseText)
-				var $companyName = $('[data-js="company-name"]').get()
-				var $companyPhone = $('[data-js="company-phone"]').get()
-				$companyName.textContent = data.name
-				$companyPhone.textContent = data.phone
-			},
-
-			isReady: function isReady() {
-				return this.readyState === 4 && this.status === 200
+				return $fragment
 			}
 		}
 	})()
 
 	app.init()
-})(window.DOM, document)
+})(window.DOM)
